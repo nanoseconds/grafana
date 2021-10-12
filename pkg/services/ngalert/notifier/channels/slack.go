d2172dd8a8c4fc15367fbe9ebe2a7bf447a9787c
Ying WANG <ying.wang@grafana.com>
2021-10-12 14:21:19 +0200

    keep the warn and crit in the interface, but stop using it in xorm

--- pkg/services/ngalert/notifier/channels/slack.go
+++ pkg/services/ngalert/notifier/channels/slack.go
@@ -210,7 +210,7 @@ var sendSlackRequest = func(request *http.Request, logger log.Logger) error {
 	}
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
-			logger.Warn("Failed to close response body", "err", err)
+			logger.Info("Failed to close response body", "err", err)
 		}
 	}()
 
@@ -220,7 +220,7 @@ var sendSlackRequest = func(request *http.Request, logger log.Logger) error {
 	}
 
 	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
-		logger.Warn("Slack API request failed", "url", request.URL.String(), "statusCode", resp.Status, "body", string(body))
+		logger.Info("Slack API request failed", "url", request.URL.String(), "statusCode", resp.Status, "body", string(body))
 		return fmt.Errorf("request to Slack API failed with status code %d", resp.StatusCode)
 	}
 
@@ -231,7 +231,7 @@ var sendSlackRequest = func(request *http.Request, logger log.Logger) error {
 	}{}
 	if err := json.Unmarshal(body, &rslt); err == nil {
 		if !rslt.Ok && rslt.Err != "" {
-			logger.Warn("Sending Slack API request failed", "url", request.URL.String(), "statusCode", resp.Status,
+			logger.Info("Sending Slack API request failed", "url", request.URL.String(), "statusCode", resp.Status,
 				"err", rslt.Err)
 			return fmt.Errorf("failed to make Slack API request: %s", rslt.Err)
 		}
